ff16a3674cd7157aa46b36cc7419ddd329422f2f
Brice Rising <brice.r.rising@gmail.com>
2021-02-25 00:44:52 -0500

    Stop saving helm templates for kong
    This includes postgres passwords

--- backend/kong/setup.sh
+++ backend/kong/setup.sh
@@ -7,6 +7,4 @@ helm repo update
 
 kubectl create namespace kong 2>/dev/null || true
 
-helm template -n kong kong kong/kong -f "${SCRIPT_PATH}/values.yaml" > "${SCRIPT_PATH}/kong.yaml"
-
 helm upgrade --install -n kong kong kong/kong -f "${SCRIPT_PATH}/values.yaml"
