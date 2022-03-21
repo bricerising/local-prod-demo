3ba47afacaacd0275339756f1fbbb201bfe66e8e
bricerising <rising.brice@gmail.com>
2022-03-21 15:08:50 -0400

    Update setup.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -4,11 +4,11 @@ set -e
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 LIFECYCLE="${1-local}"
 
-if [ 'local' == "${LIFECYCLE}" ]; then
-    "${SCRIPTPATH}"/microk8s/setup.sh
-elif [ 'dev' == "${LIFECYCLE}" ]; then
-    "${SCRIPTPATH}"/eks/setup.sh
-fi
+#if [ 'local' == "${LIFECYCLE}" ]; then
+#    "${SCRIPTPATH}"/microk8s/setup.sh
+#elif [ 'dev' == "${LIFECYCLE}" ]; then
+#    "${SCRIPTPATH}"/eks/setup.sh
+#fi
 
 #"${SCRIPTPATH}"/certManager/setup.sh "${LIFECYCLE}"
 "${SCRIPTPATH}"/externalDns/setup.sh
